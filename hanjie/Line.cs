e097d2386d21b6166ff80bf06233612feda818a2
Sampaio Nuno <nsampaio1993@gmail.com>
2014-02-24 09:13:37 +0100

    Puzzle and line class
    love u

---
+++ hanjie/Line.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hanjie
+{
+    class Line
+    {
+        private int _index;
+
+        public int Index
+        {
+            get { return _index; }
+            set { _index = value; }
+        }
+
+        private int _flush;
+
+        public int Flush
+        {
+            get { return _flush; }
+            set { _flush = value; }
+        }
+
+        private int _nbIndice;
+
+        public int NbIndice
+        {
+            get { return _nbIndice; }
+            set { _nbIndice = value; }
+        }
+
+        private int _separator;
+
+        public int Separator
+        {
+            get { return _separator; }
+            set { _separator = value; }
+        }
+
+        public Line()
+        {
+
+        }
+    }
+}
---
+++ hanjie/Puzzle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hanjie
+{
+    class Puzzle
+    {
+
+        private List<Line> _lines;
+
+        internal List<Line> Lines
+        {
+            get { return _lines; }
+            set { _lines = value; }
+        }
+    }
+}
